5aaa797702366c1eae9c24dce0821cbfce9f0c51
kunal047 <chauhan.kunal4@gmail.com>
2016-08-11 21:19:25 +0530

    Initial commit

---
+++ src/hello/HelloServlet.java
@@ -0,0 +1,26 @@
+package hello;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by rusk on 11/8/16.
+ */
+@WebServlet(name = "HelloServlet", urlPatterns = {"/hello"})
+public class HelloServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h1> Hello Everyone ! </h1>");
+        out.flush();
+    }
+}
---
+++ web/hello.jsp
@@ -0,0 +1,31 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: rusk
+  Date: 11/8/16
+  Time: 7:44 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>First One</title>
+</head>
+<body>
+
+<%
+//    int a = 3;
+//    int b = 4;
+//    int c = a + b;
+//    out.println("c = " + c);
+//    java.util.Date today = new java.util.Date();
+//    String text = "Today's date is " + today.toString();
+    String text = "Today's date is ";
+    out.println(text);
+%>
+<%=
+    new java.util.Date()
+
+%>
+
+</body>
+</html>
